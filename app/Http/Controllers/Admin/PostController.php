07d9003d8599715a280520b1cf6ddd60ca19122e
Rocco Orefice <rocco.orefice92@gmail.com>
2021-03-01 17:11:08 +0100

    delete method complete (post model)

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -93,6 +93,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin.posts.index');
     }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -21,10 +21,44 @@
             <td>{{ $post->body }}</td>
             <td>{{ $post->slug }}</td>
             <td>
+                {{-- view --}}
                 <a href="{{route('admin.posts.show', ['post' => $post->slug])}}" class="btn btn-primary p-1 m-1">View</a>
+
+                {{-- edit --}}
                 <a href="{{route('admin.posts.edit', ['post' => $post->slug])}}" class="btn btn-primary p-1 m-1">Edit
                 </a>
-                <a href="#" class="btn btn-primary p-1 m-1">Delete</a>
+                
+                {{-- delete --}}
+                <button type="button" class="btn btn-danger p-1 m-1" data-toggle="modal" data-target="#destroy-{{$post->slug}}">
+                Delete
+                </button>
+
+                {{-- modal to delete post --}}
+                <div class="modal fade" id="destroy-{{$post->slug}}" tabindex="-1" role="dialog" aria-labelledby="post-destroy-{{$post->slug}}" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="post-destroy-{{$post->slug}}">Delete Post: "{{$post->title}}"?</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Are you sure?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                <form action="{{route('admin.posts.destroy', ['post' => $post->slug])}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">
+                                        Yes
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </td>
         </tr>
         @endforeach
